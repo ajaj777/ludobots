9155e68fa3271157eee5451bf18be6a4675bb1c3
AJ Hesby <aj.hesby@gmail.com>
2023-01-09 18:09:49 -0600

    single tower of boxes

--- generate.py
+++ generate.py
@@ -1,6 +1,12 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
-
+pyrosim.Start_SDF("boxes.sdf")
+length = 1
+width = 1
+height = 1
+x = 0
+y  = 0
+z = 0.5
+for i in range(10):
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z+i] , size=[length*(0.9**i),width*(0.9**i),height*(0.9**i)])
 pyrosim.End()
