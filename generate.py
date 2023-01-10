9e4fc94d62eef239d095c5d719ebfbfeb7edb218
AJ Hesby <aj.hesby@gmail.com>
2023-01-09 18:38:35 -0600

    Part E through step 20

--- generate.py
+++ generate.py
@@ -1,14 +1,23 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+# globals 
 length = 1
 width = 1
 height = 1
 x = 0
 y  = 0
 z = 0.5
-for i in range(5):
-    for j in range(5):
-        for k in range(5):
-            pyrosim.Send_Cube(name=f"Box{i}{j}{k}", pos=[x+j,y+k,z+i] , size=[length*(0.9**i),width*(0.9**i),height*(0.9**i)])
-pyrosim.End()
+# end globals
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.End()
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[-2,2,z] , size=[length,width,height])
+    pyrosim.End()
+    
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -6,9 +6,13 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+robotID = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
+
 for i in range(10000):
     p.stepSimulation()
     time.sleep(1/60)
     print(i)
 p.disconnect()
+
+
