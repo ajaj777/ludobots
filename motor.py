d220ec1cfcd1cc1329856ecc31735fbc6bdb1803
AJ Hesby <aj.hesby@gmail.com>
2023-01-15 01:07:56 -0600

    task I complete

--- motor.py
+++ motor.py
@@ -7,24 +7,24 @@ import pybullet_data
 class MOTOR:
     def __init__(self, jointName):
         self.jointName = jointName
-        self.Prepare_To_Act()
+        #self.Prepare_To_Act()
 
-    def Prepare_To_Act(self):
-        self.amplitude = c.amplitude
-        self.frequency = c.frequency
-        self.phaseOffset = c.phaseOffset
-        if self.jointName == b'Torso_FrontLeg':
-            self.frequency = c.frequency / 2
-        self.motorValues = (np.sin(np.linspace(0, 2*np.pi, c.steps) * self.frequency + self.phaseOffset)  * self.amplitude) 
+    # def Prepare_To_Act(self):
+    #     self.amplitude = c.amplitude
+    #     self.frequency = c.frequency
+    #     self.phaseOffset = c.phaseOffset
+    #     if self.jointName == b'Torso_FrontLeg':
+    #         self.frequency = c.frequency / 2
+    #     self.motorValues = (np.sin(np.linspace(0, 2*np.pi, c.steps) * self.frequency + self.phaseOffset)  * self.amplitude) 
         
 
-    def Set_Value(self,robot,t):
+    def Set_Value(self,robot, desiredAngle):
         pyrosim.Set_Motor_For_Joint(
         bodyIndex = robot,
         jointName = self.jointName,
         controlMode = p.POSITION_CONTROL,
-        targetPosition = self.motorValues[t],
+        targetPosition = desiredAngle,
         maxForce = 40)
 
-    def Save_Values(self):
-        np.save(f'data/{self.jointName}.npy', self.motorValues)
+    # def Save_Values(self):
+    #     np.save(f'data/{self.jointName}.npy', self.motorValues)
--- robot.py
+++ robot.py
@@ -36,5 +36,11 @@ class ROBOT:
         self.nn.Print()
 
     def Act(self,t):
-        for motor in self.motors:
-            self.motors[motor].Set_Value(self.robot,t)
+        for neuronName in self.nn.Get_Neuron_Names():
+            if self.nn.Is_Motor_Neuron(neuronName):
+                
+                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
+                desiredAngle = self.nn.Get_Value_Of(neuronName)
+                self.motors[bytes(jointName, 'utf-8')].Set_Value(self.robot, desiredAngle)
+                print(neuronName + ' ' + jointName + ' ' + str(desiredAngle))
+        
